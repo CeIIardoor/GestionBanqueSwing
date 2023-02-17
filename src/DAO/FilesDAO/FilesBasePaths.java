dcffa417898b2ecbb1a198fffb585b09f321f613
CeIIardoor <cellardoorweb@gmail.com>
2023-02-17 13:15:22 +0100

    Fix for jar build

--- src/DAO/FilesDAO/FilesBasePaths.java
+++ src/DAO/FilesDAO/FilesBasePaths.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.nio.file.Path;
 
 public interface FilesBasePaths {
-    Path BASE_FOLDER = new File("src\\DB\\").toPath();
+    Path BASE_FOLDER = new File(System.getProperty("user.dir") + "\\DB\\").toPath();
     File CLIENTS_PATH = new File(BASE_FOLDER + "\\clients.csv");
     File COMPTES_PATH = new File(BASE_FOLDER + "\\comptes.csv");
     File LOGS_PATH = new File(BASE_FOLDER + "\\logs.txt");
